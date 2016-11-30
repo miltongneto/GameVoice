95d64a12f195dbba439d05c12628971767f88094
amnv <amnv@cin.ufpe.br>
2016-11-30 16:02:14 -0300

    botao muda de cor para mostrar que esta gravando

--- app/src/main/java/com/projeto/milton/multimidia/ui/VoiceRecognitionTeste.java
+++ app/src/main/java/com/projeto/milton/multimidia/ui/VoiceRecognitionTeste.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,6 +18,7 @@ import android.support.v4.content.ContextCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -145,6 +147,12 @@ public class VoiceRecognitionTeste extends Activity implements RecognitionListen
     @OnClick(R.id.btn_speak)
     public void start(View v){
         recognizarOn = true;
+        Button button = (Button) findViewById(R.id.btn_speak);
+        if (((ColorDrawable) button.getBackground()).getColor() == getResources().getColor(R.color.red))
+        {
+            button.setBackgroundColor(getResources().getColor(R.color.green));
+            button.setText("Ouvindo");
+        }
         recognize();
     }
     public void stop(View v){
